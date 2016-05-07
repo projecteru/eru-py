8b645e2c323315e318d02dd25b4e3e0388f00daa
timfeirg <kkcocogogo@gmail.com>
2016-05-07 15:05:14 +0800

    bump version (#9)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eru-py',
-    version='0.0.26',
+    version='0.0.27',
     author='tonic',
     zip_safe=False,
     author_email='tonic@wolege.ca',
